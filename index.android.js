5f3bc351dc624df9162200568a4bc3132841e8c4
RudolfHug <rudolfhug199099@hotmail.com>
2016-01-08 08:30:01 +0100

    rotation issue

--- index.android.js
+++ index.android.js
@@ -1,12 +1,63 @@
-var { requireNativeComponent, PropTypes } = require('react-native');
+'use strict';
 
-var iface = {
-  name: 'ImageView',
-  propTypes: {
-    src: PropTypes.string,
-    borderRadius: PropTypes.number,
-    resizeMode: PropTypes.oneOf(['cover', 'contain', 'stretch']),
-  },
+var React = require('react-native');
+var { requireNativeComponent, PropTypes, View } = React;
+
+var NativeAndroidCameraView = requireNativeComponent('CameraViewAndroid', AndroidCameraView);
+
+class AndroidCameraView extends React.Component {
+  constructor() {
+    super();
+    this._onChange = this._onChange.bind(this);
+    this.onCaptureCompleted = null;
+  }
+
+  _onChange(event: Event) {
+    if (event.nativeEvent.type == "camera_capture") {
+      if (this.onCaptureCompleted) {
+        this.onCaptureCompleted(null, event.nativeEvent.message);
+        this.onCaptureCompleted = null;
+      }
+    } else if (event.nativeEvent.type == "barcode_capture") {
+      if (this.props.onBarCodeRead) {
+        this.props.onBarCodeRead(event.nativeEvent.message);
+      }
+    }
+  }
+
+  capture(callback) {
+    this.onCaptureCompleted = callback;
+    this._root.setNativeProps({
+      startCapture: "true"
+    })
+  }
+
+  toggleTorch(mode) {
+    this._root.setNativeProps({
+      torchMode: mode
+    })
+  }
+
+  render() {
+    return (
+      <NativeAndroidCameraView
+          ref={component => this._root = component}
+        {...this.props} onChange={this._onChange}
+        values={this.props.values} selected={this.props.selected} />
+    );
+  }
+}
+
+AndroidCameraView.propTypes = {
+  ...View.propTypes,
+  startCapture: PropTypes.string,
+  onBarCodeRead: PropTypes.func,
+  torchMode:PropTypes.bool,
 };
 
-module.exports = requireNativeComponent('RCTCamera', iface);
+AndroidCameraView.defaultProps = {
+  onBarCodeRead: null,
+  torchMode: true
+}
+
+module.exports = AndroidCameraView;
